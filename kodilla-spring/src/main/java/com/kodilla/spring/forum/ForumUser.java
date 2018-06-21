fead02dc5e656aae3093d212029785042df3ae2b
Kasia Skowron <kasiaskowron@MacBook-Air-Kasia.local>
2018-06-21 06:36:55 +0200

    10.1

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
@@ -0,0 +1,19 @@
+package com.kodilla.spring.forum;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ForumUser {
+    private String username = "John Smith";
+
+    public ForumUser() {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
+
+
+
