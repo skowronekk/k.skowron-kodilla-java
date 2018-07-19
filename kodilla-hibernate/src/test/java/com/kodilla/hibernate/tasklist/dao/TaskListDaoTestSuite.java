9c1321d52a3ab157120280c2166f8678d329a48f
Kasia Skowron <kasiaskowron@MacBook-Air-Kasia.local>
2018-07-19 22:10:17 +0200

    13.2

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -0,0 +1,37 @@
+package com.kodilla.hibernate.tasklist.dao;
+
+
+import com.kodilla.hibernate.tasklist.TaskList;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class TaskListDaoTestSuite {
+    @Autowired
+    private TaskListDao taskListDao;
+    private static final String LISTNAME = ("Test: To do list");
+
+    @Test
+    public void testTaskDaoFindListName() {
+        //Given
+        TaskList taskList = new TaskList(LISTNAME, "send email");
+        taskListDao.save(taskList);
+        String listName = taskList.getListName();
+
+        //When
+        List<TaskList> readTaskList = taskListDao.findByListName(listName);
+
+        //Then
+        Assert.assertEquals(1, readTaskList.size());
+
+    }
+}
